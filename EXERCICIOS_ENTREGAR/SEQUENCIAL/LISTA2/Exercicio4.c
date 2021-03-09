46cc3477860ed8701871109ec62a069cb39a18b4
Diovani Coelli <diovani_coelli@hotmail.com>
2021-03-09 14:24:29 -0300

    exercicio5 para entregar sequencial lista2

--- EXERCICIOS_ENTREGAR/SEQUENCIAL/LISTA2/Exercicio4.c
+++ EXERCICIOS_ENTREGAR/SEQUENCIAL/LISTA2/Exercicio4.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main() {
-//   Escreva um programa que, dados a quantidade de litros de combustível utilizada, os quilômetros
-// percorridos por um automóvel e o valor do litro de combustível, calcule quantos quilômetros o veículo
-// percorreu por litro de combustível e o valor gasto em reais por km.
+int main()
+{
+  //   Escreva um programa que, dados a quantidade de litros de combustível utilizada, os quilômetros
+  // percorridos por um automóvel e o valor do litro de combustível, calcule quantos quilômetros o veículo
+  // percorreu por litro de combustível e o valor gasto em reais por km.
   float valorCombustivel, quilometroPorLitro, gastoPorQuilometro, litrosGastos, quilometrosPercorridos;
 
   printf("Progama para calcular quilometro por litro e valor gasto por quilometro!\n");
---
+++ EXERCICIOS_ENTREGAR/SEQUENCIAL/LISTA2/Exercicio5.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+  //   Escreva um programa para ler o número de votos brancos, nulos (incluem eleitores ausentes) e válidos de
+  // uma eleição. Calcular e mostrar o percentual que cada um (brancos, nulos e válidos) representa em relação
+  // ao total de eleitores. Lembrar que os valores dos percentuais podem não ser inteiros
+  int votosValidos, votosNulos, votosBrancos;
+  float totalVotos, percentualValido, percentualNulo, percentualBranco;
+
+  printf("Calcular a porcentagem de votos branco, nulos e validos\n");
+
+  printf("Informe a quantia de votos validos:\n");
+  scanf("%d", &votosValidos);
+
+  printf("Informe a quantia de votos brancos:\n");
+  scanf("%d", &votosBrancos);
+
+  printf("Informe a quantia de votos nulos:\n");
+  scanf("%d", &votosNulos);
+
+  totalVotos = votosBrancos + votosNulos + votosValidos;
+
+  percentualValido = (votosValidos / totalVotos) * 100;
+
+  percentualNulo = (votosNulos / totalVotos) * 100;
+
+  percentualBranco = (votosBrancos / totalVotos) * 100;
+
+  printf("Percentagem de votos validos: %1.1f%%\n", percentualValido);
+
+  printf("Percentagem de votos brancos: %1.1f%%\n", percentualBranco);
+
+  printf("Percentagem de votos nulos: %1.1f%%\n", percentualNulo);
+
+  return 0;
+}

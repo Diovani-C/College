49a6d57e1814b0d47abcbe8264901df55d41dd5d
Diovani Coelli <diovani_coelli@hotmail.com>
2021-03-09 15:18:22 -0300

    exercicio 8 para entregar sequencial lista2

---
+++ EXERCICIOS_ENTREGAR/SEQUENCIAL/LISTA2/Exercicio8.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+  //   Faça um programa que leia o salário bruto mensal de um funcionário, calcule e mostre os valores conforme o exemplo a seguir. Observação: É possível fazer esse programa utilizando somente três variáveis:
+  //  uma para ler o salário bruto, outra para os descontos e outra para o salário líquido.
+  float salarioBruto, salarioLiquido;
+
+  printf("Descontando impostos do salario bruto!\n");
+
+  printf("Informe o valor do salario R$:\n");
+  scanf("%f", &salarioBruto);
+
+  printf("Salario bruto: R$%1.2f\n", salarioBruto);
+
+  printf("IR (15%%): R$%1.2f\n", salarioBruto * 0.15);
+
+  printf("INSS (11%%): R$%1.2f\n", salarioBruto * 0.11);
+
+  printf("Sindicato (3%%): R$%1.2f\n", salarioBruto * 0.03);
+
+  salarioLiquido = salarioBruto - ((salarioBruto * 0.03) + (salarioBruto * 0.11) + (salarioBruto * 0.15));
+
+  printf("Salario liquido: R$%1.2f\n", salarioLiquido);
+
+  return 0;
+}

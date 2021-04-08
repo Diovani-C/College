91090a0b4d303936988e97ccce1953a77dd185e5
Diovani Coelli <diovani_coelli@hotmail.com>
2021-04-08 12:44:32 -0300

    Entregar, repeticao, lista2 exercicio3

---
+++ EXERCICIOS_ENTREGAR/REPETIÇÃO/LISTA2/Exercicio3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+//  Ler caracteres. Contar quantas vogais minúsculas são informadas. O caractere zero '0' indica o final da leitura, ou seja, finalizar a leitura quando informado esse caractere. O programa deverá informar também quantos caracteres foram digitados ao todo.
+
+int main()
+{
+  int vogaisQuantidade = 0, i, total = 0;
+  char caractere, vogais[] = {'a', 'e', 'i', 'o', 'u'};
+
+  while (1)
+  {
+
+    printf("Informe um caractere:");
+    scanf(" %c", &caractere);
+
+    if (caractere == '0')
+    {
+      break;
+    }
+
+    total += 1;
+
+    for (i = 0; i < 5; i++)
+    {
+      if (caractere == vogais[i])
+      {
+        vogaisQuantidade += 1;
+      }
+    }
+  }
+
+  printf("Foram informados um total de %d caracteres, e entre esses %d sao vogais minusculas!\n", total, vogaisQuantidade);
+
+  return 0;
+}
